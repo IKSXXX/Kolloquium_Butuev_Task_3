8db57d2526dad4c2a8c0485d5bb5ea70da87c6b9
hacker1634 <hacker221634@gmail.com>
2025-12-23 10:34:55 +0300

    размер копок

--- Kolloquium_Butuev_Task_3/MainForm.Designer.cs
+++ Kolloquium_Butuev_Task_3/MainForm.Designer.cs
@@ -25,46 +25,134 @@
             this.btnFilterByBrand = new System.Windows.Forms.Button();
             this.btnSortByPower = new System.Windows.Forms.Button();
             this.btnGroupByBodyType = new System.Windows.Forms.Button();
-
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCars)).BeginInit();
+            this.panelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvCars
+            // 
+            this.dgvCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCars.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.dgvCars.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCars.Location = new System.Drawing.Point(0, 65);
+            this.dgvCars.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvCars.Name = "dgvCars";
+            this.dgvCars.ReadOnly = true;
+            this.dgvCars.RowHeadersWidth = 51;
+            this.dgvCars.Size = new System.Drawing.Size(1067, 550);
+            this.dgvCars.TabIndex = 0;
+            // 
+            // panelButtons
+            // 
             this.panelButtons.Controls.Add(this.btnFilterByBrand);
             this.panelButtons.Controls.Add(this.btnSortByPower);
             this.panelButtons.Controls.Add(this.btnGroupByBodyType);
             this.panelButtons.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelButtons.Height = 40;
-            this.panelButtons.Padding = new System.Windows.Forms.Padding(5);
-
+            this.panelButtons.Location = new System.Drawing.Point(0, 0);
+            this.panelButtons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panelButtons.Size = new System.Drawing.Size(1067, 65);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // btnFilterByBrand
+            // 
+            this.btnFilterByBrand.Location = new System.Drawing.Point(14, 12);
+            this.btnFilterByBrand.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnFilterByBrand.Name = "btnFilterByBrand";
+            this.btnFilterByBrand.Size = new System.Drawing.Size(160, 49);
+            this.btnFilterByBrand.TabIndex = 0;
             this.btnFilterByBrand.Text = "Фильтр по марке";
-            this.btnFilterByBrand.Width = 120;
-            this.btnFilterByBrand.Height = 30;
-            this.btnFilterByBrand.Margin = new System.Windows.Forms.Padding(5);
-
+            // 
+            // btnSortByPower
+            // 
+            this.btnSortByPower.Location = new System.Drawing.Point(188, 12);
+            this.btnSortByPower.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnSortByPower.Name = "btnSortByPower";
+            this.btnSortByPower.Size = new System.Drawing.Size(160, 49);
+            this.btnSortByPower.TabIndex = 1;
             this.btnSortByPower.Text = "Сортировка по мощности";
-            this.btnSortByPower.Width = 120;
-            this.btnSortByPower.Height = 30;
-            this.btnSortByPower.Margin = new System.Windows.Forms.Padding(5);
-
+            // 
+            // btnGroupByBodyType
+            // 
+            this.btnGroupByBodyType.Location = new System.Drawing.Point(362, 12);
+            this.btnGroupByBodyType.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnGroupByBodyType.Name = "btnGroupByBodyType";
+            this.btnGroupByBodyType.Size = new System.Drawing.Size(160, 49);
+            this.btnGroupByBodyType.TabIndex = 2;
             this.btnGroupByBodyType.Text = "Группировка по кузову";
-            this.btnGroupByBodyType.Width = 120;
-            this.btnGroupByBodyType.Height = 30;
-            this.btnGroupByBodyType.Margin = new System.Windows.Forms.Padding(5);
-
-            this.dgvCars.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvCars.ReadOnly = true;
-            this.dgvCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-
-            this.dgvCars.Columns.Add("Автопарк", "Автопарк");
-            this.dgvCars.Columns.Add("Марка", "Марка");
-            this.dgvCars.Columns.Add("Макс. скорость", "Макс. скорость");
-            this.dgvCars.Columns.Add("Мощность", "Мощность");
-            this.dgvCars.Columns.Add("Тип кузова", "Тип кузова");
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Автопарк";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Марка";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Макс. скорость";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Мощность";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Тип кузова";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.Width = 125;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ClientSize = new System.Drawing.Size(1067, 615);
             this.Controls.Add(this.dgvCars);
             this.Controls.Add(this.panelButtons);
-            this.Text = "Управление автопарками";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Управление автопарками";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCars)).EndInit();
+            this.panelButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
     }
 }
